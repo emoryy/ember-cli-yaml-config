a5dda434f2264d95df9b9cafb7f25dbc996c0df6
Krzysztof Urban <krzysztof.urban@netguru.pl>
2015-10-01 19:48:22 +0200

    First working version

--- index.js
+++ index.js
@@ -2,5 +2,32 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-yaml-config'
+  name: 'ember-cli-yaml-config',
+
+  config: function(environment) {
+    var existsSync = require('exists-sync');
+    var lodash = require('lodash');
+    var path = require('path');
+    var yamljs = require('yamljs');
+
+    var configPath = path.dirname(this.project.configPath());
+    var addonOptions = (this.app && this.app.options.appConfig) || {};
+    var configFileNames = addonOptions.fileNames || ['config.yml', 'sec_config.yml'];
+
+    return configFileNames.reduce(function(acc, fileName) {
+      var configFilePath = path.join(configPath, fileName);
+      var config = {};
+
+      if(existsSync(configFilePath)) {
+        config = yamljs.load(configFilePath)[environment] || {};
+      } else {
+        if(addonOptions.warnAboutNonexistingFiles) {
+          console.warn(
+            '[YamlConfig] File ' + configFilePath + ' doesn\'t exist.'
+          );
+        }
+      }
+      return lodash.merge(acc, config);
+    }, {});
+  }
 };
---
+++ tests/dummy/app/controllers/index.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+
+import config from 'dummy/config/environment';
+
+export default Ember.Controller.extend({
+  foo: config.foo
+});
