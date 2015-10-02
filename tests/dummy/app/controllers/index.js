86cdc3b1b49b46b50623c8de39792eacf83cf689
Krzysztof Urban <krzysztof.urban@netguru.pl>
2015-10-02 13:53:44 +0200

    better sample config for dummy app

--- tests/dummy/app/controllers/index.js
+++ tests/dummy/app/controllers/index.js
@@ -3,5 +3,9 @@ import Ember from 'ember';
 import config from 'dummy/config/environment';
 
 export default Ember.Controller.extend({
-  foo: config.foo
+  override: config.override,
+  deepMerge: config.deepMerge,
+  first: config.first,
+  second: config.second,
+  array: config.array
 });
